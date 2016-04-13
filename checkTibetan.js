551bc6ba0fe750cf493988032610a00df5aef803
rickie120243 <rickie120243@gmail.com>
2016-04-13 12:48:40 +0800

    fix style

--- checkTibetan.js
+++ checkTibetan.js
@@ -1,6 +1,7 @@
-var possibleLetters = require("./possible-root-letters-sort");
-var correctlyLetters = require("./correctly-root-letters-sort")
+var possibleLetters = require('./possible-root-letters-sort');
+var correctlyLetters = require('./correctly-root-letters-sort')
 var letters;
+var tibetanRange;
 var content;
 
 var indexOfSorted = function (array, obj) {
@@ -14,9 +15,9 @@ var indexOfSorted = function (array, obj) {
     return low;
 }
 
-var dosort=function(arr){
-  return arr.sort(function(a,b){
-    return a[0]-b[0];
+var dosort=function(arr) {
+  return arr.sort(function(a, b){
+    return a[0] - b[0];
   });
 }
 
@@ -24,17 +25,19 @@ exports.checkSyllables = function(fn, index){
   var coords = [];
   if(0 === index || undefined === index) {
     letters = possibleLetters;
+    tibetanRange = /[\u0f2a-\u0fbf]+/g;
   } else if (1 === index) {
     letters = correctlyLetters;
+    tibetanRange = /[\u0f00-\u0f0a\u0f10-\u0fff]+/g;
   } else {
     return coords;
   }
   content=fn;
-  fn.replace(/[\u0f2a-\u0fbf]+/g,function(m,idx){
+  fn.replace(tibetanRange, function(m,idx){
     var index = indexOfSorted(letters,m);
-
-    if(index==-1&&!(m.substr(m.length-2)=="འི"||m.substr(m.length-2)=="འོ")){
-      coords.push([idx,m.length,m]);
+    if('ཥྚྲཱིཿཧྲཱིཿ' === m) console.log(index);
+    if(index == -1 && !(m.substr(m.length-2) == 'འི' || m.substr(m.length-2) == 'འོ')){
+      coords.push([idx, m.length, m]);
     }
   });
   return dosort(coords);
