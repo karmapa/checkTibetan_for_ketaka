d09f1c61f2dc9661a00f9436b6e67a05d5f6c197
chiehan <chieh.an.hsu@gmail.com>
2017-02-03 12:10:13 +0800

    fix makePbs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var srcFolder = process.argv[2];
-var globPatt = './' + srcFolder + '/**/*.xml';
+var globPatt = './' + srcFolder + '/**/*_*.xml';
 
 var fs = require("fs");
 var glob = require("glob");
@@ -29,12 +29,13 @@ function checkTibetanSpell(route) {
 
 function makePbs(text) {
   var delim = "~!@#";
-  var pbTexts = text.replace(/(<pb)/g, delim + "$1")
+  var pbTexts = text.replace(/(<pb.+?>)/g, delim + "$1")
     .split(delim);
 
-  return pbTexts.splice(0, 2, pbTexts[0] + "\n" + pbTexts[1])
-    .map(function(text) {
-      var pbId = /<pb id="(.+?)">/.exec(text)[1];
+  pbTexts.splice(0, 2, pbTexts[0] + "\n" + pbTexts[1]);
+
+  return pbTexts.map(function(pbText) {
+      var pbId = /<pb id="(.+?)"\/>/.exec(pbText)[1];
       return {pbId: pbId, text: text};
     });
 }
