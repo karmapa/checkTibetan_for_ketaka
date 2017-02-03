e05ad4fd5c88f31b407af1a0e06f2ccd7828a71c
chiehan <chieh.an.hsu@gmail.com>
2017-02-03 11:15:38 +0800

    makePbs

--- checkTibetan.js
+++ checkTibetan.js
@@ -1,4 +1,5 @@
 var correctlyLetters = require('./correctly-root-letters-sort');
+var delim = "~!@#"
 
 var indexOfSorted = function (array, syl) {
     var low = 0,
@@ -17,9 +18,19 @@ var dosort=function(arr) {
   });
 }
 
+function makePbs(text) {
+  var pbTexts = text.replace(/(<pb)/g, delim + "$1")
+    .split(delim)
+  return pbTexts.splice(0, 2, pbTexts[0] + "\n" + pbTexts[1])
+    .map(function(text) {
+      var pbId = /<pb id="(.+?)">/.exec(text)[1];
+      return {pbId: pbId, text: text};
+    });
+}
+
 exports.checkSyllables = function(text, index){
   var coords = [];
-
+  var pbs = makePbs(text);
   text.replace(/[\u0f00-\u0f0a\u0f12-\u0f1f\u0f34-\u0fff]+/g, function(m, idx){
     var index = indexOfSorted(correctlyLetters, m);
     if('ཥྚྲཱིཿཧྲཱིཿ' === m) console.log(index);
