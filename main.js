df68a512f26456c00c55e7e0b7cc1fd5b9d875a2
chiehan <chieh.an.hsu@gmail.com>
2017-02-03 10:47:27 +0800

    process files

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-var fs=require("fs");
-var checkTibetan=require("./checkTibetan.js");
-var content=fs.readFileSync("./spellchecktest.xml","utf8");
-var out=checkTibetan.checkSyllables(content);
-console.log(out);
+var srcFolder = process.argv[2];
+var globPatt = './' + srcFolder + '/**/*.xml';
+
+var fs = require("fs");
+var glob = require("glob");
+var naturalSort = require("javascript-natural-sort");
+var checkTibetan = require("./checkTibetan.js");
+
+var checkResults = glob.sync(globPatt)
+  .sort(naturalSort)
+  .map(checkTibetanSpell)
+  .filter(function(obj) {
+    return obj !== undefined;
+  });
+
+function checkTibetanSpell(route) {
+  var text = fs.readFileSync(route, "utf8");
+  var wrongSpells = checkTibetan.checkSyllables(content);
+  if (wrongSpells.length > 0) {
+    return {file: route, wrongSpells: wrongSpells};
+  }
+}
+
+fs.writeFileSync("./wrongSpells.txt", JSON.Stringify(checkResults, null, '  '), 'utf8');
