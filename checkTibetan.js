78d257f919a3fa35cdfb69929b190b760e53323e
chiehan <chieh.an.hsu@gmail.com>
2018-12-06 15:39:00 +0800

    modify tibetan letters range

--- checkTibetan.js
+++ checkTibetan.js
@@ -21,7 +21,7 @@ var dosort=function(arr) {
 exports.checkSyllables = function(fn, index){
   var coords = [];
   content = fn;
-  fn.replace(/[\u0f00-\u0f0a\u0f12-\u0f1f\u0f34-\u0fff]+/g, function(m, idx){
+  fn.replace(/[\u0f00-\u0f03\u0f40-\u0fbc]+/g, function(m, idx){
     var index = indexOfSorted(correctlyLetters, m);
     if('ཥྚྲཱིཿཧྲཱིཿ' === m) console.log(index);
     if(index == -1 && !(m.substr(m.length-2) == 'འི' || m.substr(m.length-2) == 'འོ')){
